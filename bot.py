68cdbb16cffbffd30489ea0ec4ff541e2fbae115
stucklez <alexander.stuckler@gmail.com>
2020-04-01 09:32:31 +0200

    ups

--- bot.py
+++ bot.py
@@ -168,6 +168,7 @@ async def list_movies(ctx):
         if i.server == ctx.message.guild.id:
             send_string += i.title + "\n"
     await ctx.channel.send(f'List of movies\n{send_string}')
+
 @bot.command()
 async def add_movie(ctx):
     movie = hp.get_movie(ctx.message.content)
